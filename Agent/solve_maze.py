e93eb6d1f706faae06e121c5f9b3b44bf4e227a0
DamnKuldeep <125608957+DamnKuldeep@users.noreply.github.com>
2023-05-07 18:16:51 +0530

    Update solve_maze.py

--- Agent/solve_maze.py
+++ Agent/solve_maze.py
@@ -1,10 +1,12 @@
+#define the agent
+
 class agent:
     def __init__(self,env):
         self.q_table = np.zeros((env.observation_space.shape[0], env.observation_space.shape[1], len(env.action_space)))
         
-        alpha = 0.1  
-        gamma = 0.99  
-        epsilon = 1.0  
+        alpha = 0.1  #learning rat
+        gamma = 0.99  #discount factor
+        epsilon = 1.0  #for greedy policy
         max_epsilon = 1.0
         min_epsilon = 0.01
         decay = 0.001
@@ -16,10 +18,10 @@ class agent:
             done = False
             
             for j in range(200):
-                if np.random.uniform() < epsilon:
+                if np.random.uniform() < epsilon:  #exploring the env
                     action = random.choice(env.action_space)
                 else:
-                    action = np.argmax(self.q_table[env.agent_pos[0], env.agent_pos[1], :])  
+                    action = np.argmax(self.q_table[env.agent_pos[0], env.agent_pos[1], :])    #exploitation
                     
                 
                 b,c =env.agent_pos[0], env.agent_pos[1]
@@ -27,15 +29,17 @@ class agent:
                 if done:
                     break
                 
+                # Updating the q table
                 old_value = self.q_table[b , c, action]
                 next_max = np.max(self.q_table[env.agent_pos[0], env.agent_pos[1], :])
                 new_value = (1 - alpha) * old_value + alpha * (reward + gamma * next_max)
-                self.q_table[b , c, action] = new_value
+                self.q_table[b , c, action] = new_value  
                 
-                state = next_state
-            
+                state = next_state  # updating the new_state
+            # defining epsilon decay
             epsilon = min_epsilon + (max_epsilon - min_epsilon) * np.exp(-decay *i)
-            
+     
+    # Use the final q table to solve the maze optimally
     def solve_maze(self):     
             state = env.reset()
             total_reward=0
@@ -45,20 +49,20 @@ class agent:
             self.plots= []
             while not done:
                 prev_pos = self.env.agent_pos
-                self.plots.append(plt.gcf())
-                self.env.render()
-                action = np.argmax(self.q_table[self.env.agent_pos[0], self.env.agent_pos[1], :])
-                state, reward, done, info = self.env.step(action)
-                total_reward+=reward
+                self.plots.append(plt.gcf())  # collecting the plots of agent position to make a video of it in future
+                self.env.render()  # render the current maze and agent position
+                action = np.argmax(self.q_table[self.env.agent_pos[0], self.env.agent_pos[1], :]) # choosing the optimal action from the q table for a state
+                state, reward, done, info = self.env.step(action) # taking the action
+                total_reward+=reward  # cummulating the reward
                 mem.append(self.env.agent_pos)
-                if max(mem.count(item) for item in mem)==5:
+                if max(mem.count(item) for item in mem)==5: # if the agent moves in a repetetive pattern then break the loop and print that maze couldn't be solved
                     break
-                if self.env.agent_pos == prev_pos:
+                if self.env.agent_pos == prev_pos: # if agent is stuck inside the maze and no further move is possible then say that maze is not solvable
                     i+=1
                     if i==10:
                         break
-                time.sleep(0.5)
-                display.clear_output(wait=True)
+                time.sleep(0.5) #time delay between rendering the plots
+                display.clear_output(wait=True) # clearing the display
                 
             if done:
                 self.plots.append(plt.gcf())
@@ -70,6 +74,6 @@ class agent:
             
         
 
-Agent=agent(env)
+Agent=agent(env)  # calling the agent and training it for optimal policy
 
-Agent.solve_maze()
+Agent.solve_maze() # show thw agent solving the maze using optimal policy
